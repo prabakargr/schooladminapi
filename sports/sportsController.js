34bcae1f34203af70d13a900bc150d8d5496aa2c
gowthamrn4 <gowthamrn4@gmail.com>
2018-12-04 13:10:29 +0530

    test

--- sports/sportsController.js
+++ sports/sportsController.js
@@ -86,15 +86,7 @@ var findByStandard =function(req,res){
 var updateSports = function(req, res) {
     var _id = req.params._id;
     var JoinDateSports = req.body.JoinDateSports;
-    var aadharnumberject = req.body.aadharnumber;
-    var bloodgroup = req.body.bloodgroup;
-    var dob = req.body.dob;
-    var doj = req.body.doj;
-    var fathername = req.body.fathername;
-    var fatheroccupation = req.body.fatheroccupation;
     var height = req.body.height;
-    var mothername = req.body.mothername;
-    var motheroccupation = req.body.motheroccupation;
     var name = req.body.name;
     var standard = req.standard;
     var weight =req.weight;
@@ -102,8 +94,8 @@ var updateSports = function(req, res) {
     var sportsname = req.sportsname
     console.log(_id);
 
-    sportsModel.findByIdAndUpdate({ _id }, { JoinDateSports,aadharnumberject,bloodgroup, dob,doj, fathername,fatheroccupation,height,
-        mothername,motheroccupation,name,standard,weight,gender,sportsname},
+    sportsModel.findByIdAndUpdate({ _id }, { JoinDateSports,height,
+     name,standard,weight,gender,sportsname},
         function(err,sports) {
             if (err) {
                 res.status(404).send('connot update')
--- sports/sportsModel.js
+++ sports/sportsModel.js
@@ -4,21 +4,14 @@ const Schema=mongoose.Schema
 
 const sportsModel=new Schema({
     JoinDateSports:String,
-    aadharnumber:String,
-    bloodgroup:String,
-    dob:String,
-    doj:String,
-    fathername:String,
-    fatheroccupation:String,
     height:String,
     weight:String,
-    mothername:String,
-    motheroccupation:String,
     name:String,
     standard:String,
     gender:String,
     sportsname:String,
-    studentlevel:String
+    studentlevel:String,
+    studentKey:String
 
 })
 module.exports=mongoose.model('sports',sportsModel);
